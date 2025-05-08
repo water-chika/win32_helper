1827ac008a23d292a5539b57c00aebb2e339c7ee
Water Chika <water_chika@outlook.com>
2025-05-08 16:14:40 +0800

    Add class registry_key and submodule cpp_helper

--- win32_helper.hpp
+++ win32_helper.hpp
@@ -41,6 +41,12 @@ void AdjustWindowRect(RECT*,...){}
 #include <thread>
 #include <mutex>
 #include <latch>
+#include <vector>
+#include <variant>
+#include <span>
+#include <concepts>
+
+#include <cpp_helper.hpp>
 
 namespace win32_helper{
 class Window {
@@ -188,5 +194,110 @@ static inline auto query_display_config(uint32_t flags) {
 
 	return std::pair{ std::move(path_infos), std::move(mode_infos) };
 }
+
+
+using cpp_helper::overloads;
+
+template<typename T>
+concept string_or_wide_string = requires (T t) {
+	t.data();
+	t.size();
+};
+
+static inline auto registry_open_key(HKEY key, const string_or_wide_string auto& sub_key, DWORD options, REGSAM desired) {
+	HKEY out;
+	auto res = overloads{ RegOpenKeyExW, RegOpenKeyExA }(key, sub_key.data(), options, desired, &out);
+	if (res != ERROR_SUCCESS) {
+		throw std::runtime_error{ "RegOpenKeyExW fail" };
+	}
+	return out;
+}
+
+static inline auto registry_close_key(HKEY key) {
+	auto res = RegCloseKey(key);
+	if (res != ERROR_SUCCESS) {
+		throw std::runtime_error{ "RegCloseKey fail" };
+	}
+}
+
+static inline auto foreach_registry_sub_key_name(HKEY key, auto fun) {
+	std::vector<wchar_t> name(64);
+	auto count = static_cast<DWORD>(name.size());
+	DWORD i = 0;
+	do {
+		count = name.size();
+		auto res = RegEnumKeyExW(key, i, name.data(), &count, nullptr, nullptr, nullptr, nullptr);
+		if (res == ERROR_MORE_DATA) {
+			name.resize(count);
+			res = RegEnumKeyExW(key, i, name.data(), &count, nullptr, nullptr, nullptr, nullptr);
+		}
+		if (res == ERROR_SUCCESS) {
+			fun(name);
+		}
+		else if (res == ERROR_NO_MORE_ITEMS) {
+			break;
+		}
+		else {
+			throw std::runtime_error{ "RegEnumKeyExW fail" };
+		}
+		++i;
+	} while (true);
+}
+
+static inline auto registry_query_value(HKEY key, string_or_wide_string auto& name) {
+	std::vector<uint8_t> data_buf(64);
+	DWORD data_type{}, data_size{ static_cast<DWORD>(data_buf.size()) };
+	auto res = overloads{ RegQueryValueExW, RegQueryValueExA}(key, name.data(), nullptr, &data_type, data_buf.data(), &data_size);
+	if (res == ERROR_MORE_DATA) {
+		data_buf.resize(data_size);
+		res = overloads{ RegQueryValueExW, RegQueryValueExA }(key, name.data(), nullptr, &data_type, data_buf.data(), &data_size);
+	}
+	if (res != ERROR_SUCCESS) {
+		throw std::runtime_error{ "RegQueryValueExW fail" };
+	}
+
+	std::variant<DWORD, std::string, std::vector<uint8_t>> data{};
+	if (data_type == REG_DWORD) {
+		data = *reinterpret_cast<DWORD*>(data_buf.data());
+	}
+	else if (data_type == REG_SZ) {
+		data = std::string{ reinterpret_cast<const char*>(data_buf.data()) };
+	}
+	else if (data_type == REG_BINARY) {
+		data = data_buf;
+	}
+	else {
+		throw std::runtime_error{ "unimplemented data type" };
+	}
+
+	return data;
+}
+
+class registry_key {
+public:
+	registry_key(HKEY parent, const string_or_wide_string auto& key_name, DWORD options, REGSAM desired)
+		:
+		m_key{registry_open_key(parent, key_name, options, desired)}
+	{}
+	~registry_key() {
+		registry_close_key(m_key);
+	}
+
+	auto get_key() {
+		return m_key;
+	}
+
+	auto query_value(const string_or_wide_string auto& key_name) {
+		return registry_query_value(m_key, key_name);
+	}
+
+	auto foreach_sub_key_name(auto fun) {
+		foreach_registry_sub_key_name(m_key,
+			std::move(fun));
+	}
+
+private:
+	HKEY m_key;
+};
 
 }
